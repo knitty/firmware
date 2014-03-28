b6ff94654db2dcfcabc4284b007c8555238bf0d5
schinken <schinken@hackerspace-bamberg.de>
2014-03-28 23:50:50 +0100

    Add serial protocol

--- software/Knitty/Knitty.ino
+++ software/Knitty/Knitty.ino
@@ -1,48 +1,69 @@
+//////////////////////////////////////////////////////////////////////////////
+// Knitty Project
+//
+// Author: ptflea, schinken
+//
 
-#define INT_0       0
-#define INT_1       1
+#define INT_0         0
+#define INT_ENCODER   INT_0
+
+//////////////////////////////////////////////////////////////////////////////
+// General purpose definitions
 
 #define PIN_IFDR      3          // Green
 #define PIN_CSENSE    2          // Yellow
 #define PIN_CREF      4          // White
 #define PIN_SEL       9          // Blue,  Pattern
-
-#define PIN_BUTTON    12
-
-#define INT_IFDR      INT_1
-#define INT_ENCODER   INT_0
-
+#define PIN_NEEDLE    PIN_SEL
 
 #define DIRECTION_UNKNOWN       0
 #define DIRECTION_LEFT_RIGHT   -1
 #define DIRECTION_RIGHT_LEFT    1
 
-
-#define PIN_NEEDLE       PIN_SEL
-#define NEEDLE_ACTIVE    HIGH
-#define NEEDLE_INACTIVE  LOW
-
-const unsigned char PATTERN_LENGTH = 30;
-
 char currentDirection = DIRECTION_UNKNOWN;
 char lastDirection = DIRECTION_UNKNOWN;
 
-signed int currentCursorPosition = -14;
+signed int currentCursorPosition = -15;
 signed int leftEndCursorPosition = 0;
 unsigned int currentPatternIndex = 0;
 
-//char knitPattern[PATTERN_LENGTH] = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};
-char knitPattern[PATTERN_LENGTH] = {1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1};
+unsigned char knitPattern[255] = {0};
+
+//////////////////////////////////////////////////////////////////////////////
+// Knitty Serial Protocol
+
+// Receive commands
+#define COM_CMD_PATTERN      'P'
+#define COM_CMD_CURSOR       'C'
+#define COM_CMD_SEPERATOR    ':'
+
+// Send commands
+#define COM_CMD_PATTERN_END  'E'
+#define COM_CMD_DIRECTION    'D'
+#define COM_CMD_DEBUG        'V'
+
+#define COM_CMD_PLOAD_END    '\n'
+
+// Parser states
+#define COM_PARSE_CMD      0x01
+#define COM_PARSE_SEP      0x02
+#define COM_PARSE_PLOAD    0x03
+
+
+unsigned char parserState = COM_PARSE_CMD;
+
+unsigned char parserReceivedCommand = 0;
+char parserReceivedPayload[255] = {0};
+size_t parserReceivedBytes = 0;
+
+
+unsigned char patternLength = 0;
 
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Hallo Knitty, hello World!");
 
-  // Setup CURSOR pin change interrupt
-  //pinMode(PIN_IFDR, INPUT);
-  //attachInterrupt(INT_IFDR, interruptPinChangeCarriageConnect, RISING);
-  
   // Setup PHOTO SENSOR pin change interrupt
   pinMode(PIN_CSENSE, INPUT);
   pinMode(PIN_CREF, INPUT);
@@ -51,25 +72,85 @@ void setup() {
   // Setup Needle
   pinMode(PIN_NEEDLE, OUTPUT);  
   digitalWrite(PIN_NEEDLE, LOW);
+}
+
+void executeCommand(unsigned char cmd, char *payload, size_t length) {
   
-  // Setup HID
-  pinMode(PIN_BUTTON, INPUT);
+  switch(cmd) {
+    case COM_CMD_PATTERN:
+      memset(knitPattern, 0,255);
+      
+      for(unsigned char i = 0; i < length; i++) {
+        knitPattern[i] = (payload[i] == '1')? 1 : 0;
+      }
+      
+      patternLength = length;
+      
+      break;
+      
+    case COM_CMD_CURSOR:
+      currentCursorPosition = atoi(payload);
+      break;
+  }
+}
+
+void parserSerialStream() {
+  
+  if (Serial.available() == 0) {
+    return;
+  }
+  
+  char buffer = Serial.read();
+    
+  switch(parserState) {
+      
+    case COM_PARSE_CMD:
+        if(buffer == COM_CMD_PATTERN || buffer == COM_CMD_CURSOR) {
+          parserState = COM_PARSE_SEP;
+          parserReceivedCommand = buffer;
+          parserReceivedBytes = 0;
+        }
+      break;
+      
+    case COM_PARSE_SEP:
+      
+      // We're awaiting a seperator here, if not, back to cmd
+      if(buffer == COM_CMD_SEPERATOR) {
+        parserState = COM_PARSE_PLOAD;
+        break;
+      }
+        
+      parserState = COM_PARSE_CMD;
+      break;
+        
+    case COM_PARSE_PLOAD:
+      
+      if(buffer == COM_CMD_PLOAD_END) {
+        // Everything is read, execute command
+        parserReceivedPayload[parserReceivedBytes+1] = '\0';
+        
+        executeCommand(parserReceivedCommand, parserReceivedPayload, parserReceivedBytes);
+        parserState = COM_PARSE_CMD;
+        
+        Serial.println("OK");
+        break;
+      }
+        
+      parserReceivedPayload[parserReceivedBytes] = buffer;
+      parserReceivedBytes++;
+          
+      break;        
+  } 
 }
 
 void loop() {
-  char currentButtonStatus = digitalRead(PIN_BUTTON);
-  if(currentButtonStatus == 1) {    
-    Serial.println("Reset cursor counter to zero");
-    currentCursorPosition = 0;
-    currentPatternIndex = 0;
-  }
+  parserSerialStream();
 }
 
 void setNeedleByCursor(char cursorPosition) {
  
-  if(cursorPosition > PATTERN_LENGTH-1 && cursorPosition < 0) {
-    Serial.print("Warning! Needle out of range");
-    Serial.println(cursorPosition, 10);
+  // Just to be sure that we never exceed the pattern
+  if(cursorPosition > patternLength-1 && cursorPosition < 0) {
     return;
   }
  
@@ -79,10 +160,8 @@ void setNeedleByCursor(char cursorPosition) {
 void setNeedle(char state) {  
   if(state == 1) {
     digitalWrite(PIN_NEEDLE, HIGH);
-    Serial.print("HIGH");
   } else {
     digitalWrite(PIN_NEEDLE, LOW); 
-   Serial.print("LOW"); 
   }
 }
 
@@ -100,26 +179,20 @@ void interruptPinChangeEncoder() {
   
   // RTL = 1, LTR = -1
   currentCursorPosition += currentDirection; 
-  Serial.println(currentCursorPosition, 10);
   
   // Check if we're in range of our needles
   if((currentDirection == DIRECTION_RIGHT_LEFT && currentCursorPosition > 0) ||
      (currentDirection == DIRECTION_LEFT_RIGHT && currentCursorPosition <= leftEndCursorPosition)) {
-        
-    Serial.print("PI: ");
-    Serial.println(currentPatternIndex, 10);
 
-    if(currentPatternIndex >= PATTERN_LENGTH-1) {
+    if(currentPatternIndex >= patternLength-1) {
          
        setNeedle(0);
        currentPatternIndex = 0;
        
        if(currentDirection == DIRECTION_RIGHT_LEFT) {
-         Serial.println("RTL Pattern finished!");
          leftEndCursorPosition = currentCursorPosition-1;
-       } else {
-         Serial.println("LTR Pattern finished!");
        }
+       
     } else {
           
       if(currentDirection == DIRECTION_RIGHT_LEFT && currentPinChangeValue == 0) {
@@ -139,8 +212,3 @@ void interruptPinChangeEncoder() {
     currentPatternIndex = 0;
   }
 }
-
-void interruptPinChangeCarriageConnect() {  
-  Serial.println("IFDR up");
-}
-
